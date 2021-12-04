8efefae2abbde7f2cfec7bc5a78b6d35f720462f
ANURAG SINGH <54910091+anurags10@users.noreply.github.com>
2021-12-04 12:39:55 +0530

    Add files via upload

---
+++ About.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component.
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem natus ab quam blanditiis temporibus voluptatem, rerum dolore aliquam reprehenderit ea iste, quasi quisquam sunt id quo voluptates accusamus praesentium dolorem! Laborum atque unde quod!</p>
+        </div>
+    )
+}
---
+++ Todos.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Todo } from "./Todo";
+
+export const Todos = (props) => {
+  let myStyle = {
+    minHeight: "70vh",
+    margin: "10px auto",
+  };
+  return (
+    <div className="container" style={myStyle}>
+      <h3 className="my-3">Todos List</h3>
+      {props.todos.length === 0
+        ? "No Todos To Display"
+        : props.todos.map((todo) => {
+            return (
+              <Todo todo={todo} key={todo.sno} onDelete={props.onDelete} />
+            );
+          })}
+    </div>
+  );
+};
